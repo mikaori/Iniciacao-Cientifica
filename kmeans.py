20b994991025e82d3e41ea0442f6e6885499550f
Michelle Kaori Hamada <33937058+mikaori@users.noreply.github.com>
2018-08-28 00:57:43 -0300

    Update kmeans.py
    - Tornou o arquivo de entrada que está hardcoded em um parâmetro
    - Determinou o número de iterações necessárias automaticamente (deve parar quando os centroides não mudarem mais ou quando um número máximo de iterações for atingido)

--- kmeans.py
+++ kmeans.py
@@ -16,87 +16,96 @@ def dist_euclidiana (ponto1,ponto2):
 	return math.sqrt(soma)
 
 def sumColumn(matrix):
-    return np.sum(matrix, axis=0)
+	return np.sum(matrix, axis=0)
 
 def calcularPontoMedio(lista):
-    somaColunas,pontoMedio=[], []
-    tam=len(lista)
-    for i in range(2):
-        somaColunas=sumColumn(lista)
-        pontoMedio.append(somaColunas[i]/tam)
-    return(pontoMedio)
+	somaColunas,pontoMedio=[], []
+	tam=len(lista)
+	for i in range(2):
+		somaColunas=sumColumn(lista)
+		pontoMedio.append(somaColunas[i]/tam)
+	return(pontoMedio)
 
 def plotarPontos(df, numero):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(xs=numero.p1, ys=numero.p2, zs=numero.p3, c='r', marker = 'o')
-    ax.scatter(xs=df.p1, ys=df.p2, zs=df.p3, marker= '^')
-    plt.show()
-
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	ax.scatter(xs=numero.p1, ys=numero.p2, zs=numero.p3, c='r', marker = 'o')
+	ax.scatter(xs=df.p1, ys=df.p2, zs=df.p3, marker= '^')
+	plt.show()
 
 def pontoStringFloat (lista):
-    return list(map(float, lista))
+	return list(map(float, lista))
 
 def comparaValores(lista, valor):
-    index = 0;
-    for i in range(len(lista)):
-        if lista[i] == valor:
-            index=i
-    return index
+	index = 0;
+	for i in range(len(lista)):
+		if lista[i] == valor:
+			index=i
+	return index
 
-with open('dadosteste.csv', 'r') as arquivo:
-    leitor = csv.reader(arquivo, delimiter=',')
-    dados = []
-    for ponto in leitor:
-        dados.append(ponto)
-    dados = dados[1:]
-    dados = list(map(pontoStringFloat, dados))
-    centroides= dados[0:3]
-    print ("Centroides ", centroides)
-    contador =0;
+def lerArquivo(arquivo):
+	with open('dadosteste.csv', 'r') as arquivo:
+		leitor = csv.reader(arquivo, delimiter=',')
+		dados = []
+		for ponto in leitor:
+			dados.append(ponto)
+		dados = dados[1:]
+		dados = list(map(pontoStringFloat, dados))
+	return dados
 
-for t in range(20):
-    contador=contador +1
-    distancias = [[] for i in range(len(centroides))]
-    newDistancias=[[] for i in range(len(centroides))]
-    cluster1,cluster2,cluster3,fakePoint=[],[],[],[]
+dados = lerArquivo('dados1.csv')
+n = int(input('Qual o numero de centroides? '))
+centroides = dados[0:n]
+contador, t, achei = 0, 0, 0
+ultimosCentroides = centroides
 
-    for j in range (len(centroides)):
-        for i in range(len(dados)):
-            distancias[j].append(dist_euclidiana(centroides[j],  dados[i]))
+while (t < 20 and contador < 5):
+	distancias = [[] for i in range(len(centroides))]
+	newDistancias=[[] for i in range(len(centroides))]
+	cluster1,cluster2,cluster3,fakePoint=[],[],[],[]
 
-    for i in range(len(dados)):
-        if distancias[0][i]<distancias[1][i] and distancias[0][i]<distancias[2][i]:
-            cluster1.append(dados[i])
-        elif distancias[1][i]<distancias[0][i] and distancias[1][i]<distancias[2][i]:
-            cluster2.append(dados[i])
-            #print(cluster2)
-        else:
-            cluster3.append(dados[i])
-    #print(cluster1)
-    #print(cluster2)
-    #print(cluster3)
-    fakePoint.append(calcularPontoMedio(cluster1))
-    fakePoint.append(calcularPontoMedio(cluster2))
-    fakePoint.append(calcularPontoMedio(cluster3))
-    temp = 0;
-    #print (fakePoint)
-    for j in range(len(fakePoint)):
-        for i in range(len(dados)):
-            temp = dist_euclidiana(fakePoint[j], dados[i])
-            #print (temp)
-            newDistancias[j].append(temp)
+	for j in range (len(centroides)):
+		if (centroides[j] == ultimosCentroides[j]):
+			achei = 1;
+
+	if achei!=0:
+		contador = contador+1
+
+	ultimosCentroides=centroides #armazena o valoe do ultimo centroide
+
+	for j in range (len(centroides)):
+		for i in range(len(dados)):
+			distancias[j].append(dist_euclidiana(centroides[j],  dados[i]))
+
+	cluster = [[] for i in range(len(dados))]
+
+	for i in range(len(dados)):
+		if ((distancias[0][i]<distancias[1][i]) and (distancias[0][i]<distancias[2][i])):
+			cluster[0].append(dados[i])
+		elif ((distancias[1][i]<distancias[0][i]) and (distancias[1][i]<distancias[2][i])):
+			cluster[1].append(dados[i])
+		else:
+			cluster[2].append(dados[i])
+
+	fakePoint.append(calcularPontoMedio(cluster[0]))
+	fakePoint.append(calcularPontoMedio(cluster[1]))
+	fakePoint.append(calcularPontoMedio(cluster[2]))
+	temp = 0;
+
+	for j in range(len(fakePoint)):
+		for i in range(len(dados)):
+			temp = dist_euclidiana(fakePoint[j], dados[i])
+			newDistancias[j].append(temp)
 	#Achando os novos centroides
 	#achar o menor valor de cada lista
 	#newDistancia=[[dist ao ponto fake (1)],[dist ao ponto fake (2)],[dist ao ponto fake(3)]]
-    for i in range(len(centroides)):
-        index = comparaValores(newDistancias[i], min(newDistancias[i]))
-        print (index)
-        centroides[i] = dados[index]
+	for i in range(len(centroides)):
+		index = comparaValores(newDistancias[i], min(newDistancias[i]))
+		centroides[i] = dados[index]
 	#acho o menor valor de cada lista e armazeno em uma lista vai vai conter os 3 menores valores
 	#objetivo: comparar cada valor de menorValor[i] com os valores das 3 listas de newDistancia
 	#para achar os novos valores de centroides:
- 	#para cada i faco ele receber o valor do dado no indice encontrado pela funcao comparaValores envio para a funcao comparaValores
+	#para cada i faco ele receber o valor do dado no indice encontrado pela funcao comparaValores envio para a funcao comparaValores
 	#a lista com as novas distancias e o menor valor que encontrei dessa lista para poder entao achar o indice desse menor valor.
-
-print ("centroides finais: ",centroides, "quantas vezes rodei por aqui: ", contador)
+	t=t+1
+print ("centroides finais: ", centroides, "quantas vezes rodei por aqui: ", contador)
