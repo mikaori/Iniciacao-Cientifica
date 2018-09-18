b37895739f7d2e27a03ed116e223f0b6a8b697cd
Michelle Hamada <mchll.hmd@gmail.com>
2018-09-18 02:47:27 -0300

    corrigindo erros

--- SOM/teste.py
+++ SOM/teste.py
@@ -44,7 +44,7 @@ def d_taxa_aprendizado(taxa_aprendizado_inicial, i, interacoes):
 	return taxa_aprendizado_inicial * np.exp(-i / interacoes)
 
 def att_vizinhanca(distancia, largura): #o quanto cada neuronio sofrerá com o reajuste dos pesos
-	return np.exp(-(distancia**2) / (2* (largura**2)))
+	return np.exp(-(distancia**2) / (2*(largura**2)))
 
 def AcharMatch(peso, x): #busca neuronio vencedor
 	distancia = []
@@ -59,56 +59,51 @@ def init_largura(pesos): #como achar a largura da grade? li que era o "raio"
 		sum_peso.append(sum(pesos[i]))
 	index =comparaValores(sum_peso, max(sum_peso)) #maior ponto mais distante
 	maior_valor= pesos[index]
+	print(maior_valor)
 	index =comparaValores(sum_peso, min(sum_peso)) #menor ponto mais distante
 	menor_valor= pesos[index]
+	print(menor_valor)
 	largura = (dist_euclidiana(maior_valor,menor_valor))/2
+	print(largura)
 	return (dist_euclidiana(maior_valor,menor_valor))/2
 
-arquivo = 'dadosteste.csv'
-
-dados = lerArquivo(arquivo)
 #linha = int(input('Sua matriz será? linha = '))
 linha=3
 #coluna = int(input('coluna = '))
 coluna =3
-pesos = gerar_matriz(linha,coluna,dados)
-interacoes = int(input('numero de interacoes = '))
+pesos = [[2,2],[7,5],[1,2],[5,3],[7,4],[3,1],[6,4],[5,4],[1,1]]
 taxa_aprendizagem_inicial = 0.01
-largura_inicial = init_largura(pesos)
 taxa_aprendizagem = taxa_aprendizagem_inicial
+largura_inicial = init_largura(pesos)
 largura = largura_inicial
+
 contador =0
 
 #ULTIMO VALOR SEMPRE TA DANDO 0 DE CADA PONTO WHY
-for j in range(interacoes):
-
-	if j==0 or j%len(dados)==1:
-		dadosSortidos=dados
-		r.shuffle(dadosSortidos)
-
+for j in range(1):
+	dados =[6,5]
 	#inicializando valores iniciais
-	for i in range(len(dados)):
-		neuronio_vencedor = AcharMatch (pesos,dadosSortidos[i%len(dados)])
-		print ("neuronio_vencedor: ",neuronio_vencedor)
+	neuronio_vencedor = [7,5]
 
-		#CALCULANDO DISTANCIA LATERAL - dist do neuronio_vencedor para os outros
-		for valor in range(len(pesos)):
-			print("pesos ",pesos[valor],"neuronio_vencedor: ", neuronio_vencedor)
-			valor_distancia = dist_euclidiana(neuronio_vencedor, pesos[valor])
-			if valor_distancia >=0.00001:
-				#ATUALIZANDO A VIZINHANCA
-				if valor_distancia<=largura:
-					influencia = att_vizinhanca(valor_distancia,largura)
-					for n in range(3): #quero o numero de colunas de um ponto
-						w = pesos[valor]
-						print("w = ", w)
-						pesos[valor][n] = w[n]+(taxa_aprendizagem*influencia*(dadosSortidos[i%len(dados)][n]-w[n]))
-						print("novos pesos: ", pesos[i])
+	#CALCULANDO DISTANCIA LATERAL - dist do neuronio_vencedor para os outros
+	for valor in range(len(pesos)):
+		print("pesos ",pesos[valor],"neuronio_vencedor: ", neuronio_vencedor)
+		valor_distancia = dist_euclidiana(neuronio_vencedor, pesos[valor])
+		print("valor distancia: ", valor_distancia, " - largura: ", largura)
+		if valor_distancia>=0.00000001:
+			#ATUALIZANDO A VIZINHANCA
+			influencia = att_vizinhanca(valor_distancia,largura)
+			print("influencia: ", influencia)
+			for n in range(2): #quero o numero de colunas de um ponto
+				w = pesos[valor]
+				print("w = ", w)
+				pesos[valor][n] = w[n]+(taxa_aprendizagem*influencia*(dados[n]-w[n]))
+			print("novos pesos: ", pesos[valor])
 
-		#ATUALIZANDO TAXA DE APRENDIZAGEM
-		taxa_aprendizagem = d_taxa_aprendizado(taxa_aprendizagem_inicial,i,interacoes)
+	#ATUALIZANDO TAXA DE APRENDIZAGEM
+	taxa_aprendizagem = d_taxa_aprendizado(taxa_aprendizagem_inicial,1,1)
 
-		#ATUALIZANDO LARGURA
-		largura = d_largura(largura_inicial, i, interacoes)
+	#ATUALIZANDO LARGURA
+	largura = d_largura(largura_inicial, 1, 1)
 
 print ("pesos: ", pesos)
