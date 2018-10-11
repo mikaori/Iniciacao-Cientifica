fdefd141db0867b596cc388046bad683c63ca0d1
mikaori <mchll.hmd@gmail.com>
2018-10-11 16:20:20 -0300

    adicionei def main():

--- SOM/SOM.py
+++ SOM/SOM.py
@@ -127,64 +127,67 @@ def calcularDistanciaGrade(pesos):
 			dist_grade_neuronios.append(dist_euclidiana(pesos[i], pesos[i+coluna]))
 	print ('distancia de casa ponto para os vizinhos: ', dist_grade_neuronios)
 
-dados = lerArquivo('dadosteste.csv')
+def main():
+	dados = lerArquivo('dadosteste.csv')
 
-#linha = int(input('Sua matriz será? linha = '))
-linha = 3
-#coluna = int(input('coluna = '))
-coluna = 3
+	#linha = int(input('Sua matriz será? linha = '))
+	linha = 3
+	#coluna = int(input('coluna = '))
+	coluna = 3
 
-grade = gerar_grade(linha, coluna)
-init_grade(linha, coluna, grade)
+	grade = gerar_grade(linha, coluna)
+	init_grade(linha, coluna, grade)
 
-pesos = gerar_grade(linha, coluna)
-init_pesos(linha, coluna, dados, pesos)
+	pesos = gerar_grade(linha, coluna)
+	init_pesos(linha, coluna, dados, pesos)
 
-#print ("pesos: ", pesos)
-interacoes = int(input('numero de interacoes = '))
+	#print ("pesos: ", pesos)
+	interacoes = int(input('numero de interacoes = '))
 
-taxa_aprendizagem_inicial = 0.01
-largura_inicial = init_largura(pesos)
-taxa_aprendizagem = taxa_aprendizagem_inicial
-largura = largura_inicial
-contador = 0
-n_colunas = len(dados[0])
+	taxa_aprendizagem_inicial = 0.01
+	largura_inicial = init_largura(pesos)
+	taxa_aprendizagem = taxa_aprendizagem_inicial
+	largura = largura_inicial
+	contador = 0
+	n_colunas = len(dados[0])
 
-for j in range(interacoes):
+	for j in range(interacoes):
 
-	if j==0 or j%len(dados)==1:
-		dadosSortidos = dados
-		r.shuffle(dadosSortidos)
+		if j==0 or j%len(dados)==1:
+			dadosSortidos = dados
+			r.shuffle(dadosSortidos)
 
-	#inicializando valores iniciais
-	for i in range(len(dados)):
-		neuronio_vencedor = AcharMatch (pesos, dadosSortidos[i%len(dados)])
-		#print ("neuronio_vencedor: ",neuronio_vencedor)
+		#inicializando valores iniciais
+		for i in range(len(dados)):
+			neuronio_vencedor = AcharMatch (pesos, dadosSortidos[i%len(dados)])
+			#print ("neuronio_vencedor: ",neuronio_vencedor)
 
-		#CALCULANDO DISTANCIA LATERAL - dist do neuronio_vencedor para os outros
-		for valor in range(len(pesos)):
-			#print("pesos ", pesos[valor], "neuronio_vencedor: ", neuronio_vencedor)
-			valor_distancia = dist_euclidiana(neuronio_vencedor, pesos[valor])
-			if valor_distancia >= 0.00001:
-				#ATUALIZANDO A VIZINHANCA
-				if valor_distancia <= largura:
-					influencia = att_vizinhanca(valor_distancia, largura)
-					for n in range(n_colunas): #quero o numero de colunas de um ponto
-						w = pesos[valor]
-						#print("w = ", w)
-						pesos[valor][n] = w[n]+(taxa_aprendizagem*influencia*(dadosSortidos[i%len(dados)][n]-w[n]))
-						#print("novos pesos: ", pesos[i])
+			#CALCULANDO DISTANCIA LATERAL - dist do neuronio_vencedor para os outros
+			for valor in range(len(pesos)):
+				#print("pesos ", pesos[valor], "neuronio_vencedor: ", neuronio_vencedor)
+				valor_distancia = dist_euclidiana(neuronio_vencedor, pesos[valor])
+				if valor_distancia >= 0.00001:
+					#ATUALIZANDO A VIZINHANCA
+					if valor_distancia <= largura:
+						influencia = att_vizinhanca(valor_distancia, largura)
+						for n in range(n_colunas): #quero o numero de colunas de um ponto
+							w = pesos[valor]
+							#print("w = ", w)
+							pesos[valor][n] = w[n]+(taxa_aprendizagem*influencia*(dadosSortidos[i%len(dados)][n]-w[n]))
+							#print("novos pesos: ", pesos[i])
 
-		#ATUALIZANDO TAXA DE APRENDIZAGEM
-		taxa_aprendizagem = d_taxa_aprendizado(taxa_aprendizagem_inicial, i, interacoes)
-		#print("taxa aprendizagem: ",taxa_aprendizagem)
+			#ATUALIZANDO TAXA DE APRENDIZAGEM
+			taxa_aprendizagem = d_taxa_aprendizado(taxa_aprendizagem_inicial, i, interacoes)
+			#print("taxa aprendizagem: ",taxa_aprendizagem)
 
-		#ATUALIZANDO LARGURA
-		largura = d_largura(largura_inicial, i, interacoes)
-		#print("largura: ", largura)
+			#ATUALIZANDO LARGURA
+			largura = d_largura(largura_inicial, i, interacoes)
+			#print("largura: ", largura)
 
-print ('pesos: ', pesos)
+	print ('pesos: ', pesos)
 
-calcularDistanciaGrade(pesos)
+	calcularDistanciaGrade(pesos)
 
-plot3d(dados, pesos)
+	plot3d(dados, pesos)
+
+main()
