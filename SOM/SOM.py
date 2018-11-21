38b496fad113357464f85ed94662a0fb497f537f
mikaori <mchll.hmd@gmail.com>
2018-11-21 00:13:32 -0200

    plot da matriz u com hexabin

--- SOM/SOM.py
+++ SOM/SOM.py
@@ -7,6 +7,8 @@ from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 import numpy as np
 from pandas.plotting import scatter_matrix
+import copy
+
 
 def lerArquivo(arquivo):
 	with open(arquivo, 'r') as arquivo:
@@ -105,6 +107,66 @@ def plot3d(dados, pesos):
 	ax.scatter(dados.x, dados.y, dados.z, c='r', marker='d')
 	plt.show()
 
+def plotMatrizU(pesos, n_colunas):
+	dist_matriz=[]
+	neuronio=[]
+	neuronio_comp=[]
+
+
+	dist_matriz.append(dist_euclidiana(pesos[0], pesos[0]))
+	neuronio.append(0)
+	neuronio_comp.append(0)
+	dist_matriz.append(dist_euclidiana(pesos[0], pesos[1]))
+	neuronio.append(0)
+	neuronio_comp.append(1)
+	dist_matriz.append(dist_euclidiana(pesos[1], pesos[2]))
+	neuronio.append(1)
+	neuronio_comp.append(2)
+
+	dist_matriz.append(dist_euclidiana(pesos[0], pesos[3]))
+	neuronio.append(0)
+	neuronio_comp.append(4)
+	dist_matriz.append(dist_euclidiana(pesos[1], pesos[4]))
+	neuronio.append(1)
+	neuronio_comp.append(4)
+	dist_matriz.append(dist_euclidiana(pesos[2], pesos[5]))
+	neuronio.append(2)
+	neuronio_comp.append(5)
+
+	dist_matriz.append(dist_euclidiana(pesos[3], pesos[3]))
+	neuronio.append(3)
+	neuronio_comp.append(3)
+	dist_matriz.append(dist_euclidiana(pesos[3], pesos[4]))
+	neuronio.append(3)
+	neuronio_comp.append(4)
+	dist_matriz.append(dist_euclidiana(pesos[4], pesos[5]))
+	neuronio.append(4)
+	neuronio_comp.append(5)
+
+	dist_matriz.append(dist_euclidiana(pesos[3], pesos[6]))
+	neuronio.append(3)
+	neuronio_comp.append(6)
+	dist_matriz.append(dist_euclidiana(pesos[4], pesos[7]))
+	neuronio.append(4)
+	neuronio_comp.append(7)
+	dist_matriz.append(dist_euclidiana(pesos[5], pesos[8]))
+	neuronio.append(6)
+	neuronio_comp.append(8)
+
+	dist_matriz.append(dist_euclidiana(pesos[6], pesos[6]))
+	neuronio.append(6)
+	neuronio_comp.append(6)
+	dist_matriz.append(dist_euclidiana(pesos[6], pesos[7]))
+	neuronio.append(6)
+	neuronio_comp.append(7)
+	dist_matriz.append(dist_euclidiana(pesos[7], pesos[8]))
+	neuronio.append(7)
+	neuronio_comp.append(8)
+
+	df = pd.DataFrame({'neuronio': neuronio, 'neuronio comparado': neuronio_comp, 'distancia': dist_matriz})
+	ax = df.plot.hexbin(x='neuronio', y='neuronio comparado', C='distancia', gridsize=10, cmap="viridis")
+	plt.show()
+
 def main():
 	dados = lerArquivo('dadosteste.csv')
 
@@ -145,7 +207,7 @@ def main():
 			for valor in range(len(pesos)):
 				#print("pesos ", pesos[valor], "neuronio_vencedor: ", neuronio_vencedor)
 				valor_distancia = dist_euclidiana(neuronio_vencedor, pesos[valor])
-				print("\n 2 dados: ", dados, " pesos: ", pesos)
+				#print("\n 2 dados: ", dados, " pesos: ", pesos)
 				if valor_distancia >= 0.00001:
 					#ATUALIZANDO sA VIZINHANCA
 					if valor_distancia <= largura:
@@ -159,7 +221,7 @@ def main():
 							pesos[valor][n] = w[n]+(taxa_aprendizagem*influencia*(dadosSortidos[i%len(dados)][n]-w[n]))
 							#print("novos pesos: ", pesos[i])
 
-				print("\n 3 dados: ", dados, " pesos: ", pesos)
+				#print("\n 3 dados: ", dados, " pesos: ", pesos)
 			#ATUALIZANDO TAXA DE APRENDIZAGEM
 			taxa_aprendizagem = d_taxa_aprendizado(taxa_aprendizagem_inicial, i, interacoes)
 			#print("taxa aprendizagem: ",taxa_aprendizagem)
@@ -171,6 +233,7 @@ def main():
 
 	#print ('pesos: ', pesos)
 
-	plot3d(pesos, dados)
+	#plot3d(pesos, dados)
+	plotMatrizU(pesos, n_colunas)
 
 main()
